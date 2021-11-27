d618454c9122305168d3704e8c59935ad792c4a6
roman shain <urstzt@gmail.com>
2021-11-28 01:05:47 +0300

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ docker stop questionnaire
 docker rm questionnaire
 docker rmi questionnaire-img
 docker build -t questionnaire-img .
-docker run -d --name questionnaire -p 80:80 questionnaire-img
+docker run -d --name questionnaire --restart always -p 80:80 questionnaire-img
 exit
